265455a3c68cfdf21768d4688c6630bc3c5951f1
Devon Wood <wooddv35@gmail.com>
2017-12-06 15:29:10 -0500

    updated yarn file

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const session = require("express-session");
-const passport = require("passport");
 const PORT = process.env.PORT || 3001;
 const Users = require("./models/user.js");
 
@@ -13,13 +11,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Serve up static assets
 app.use(express.static("client/build"));
-app.use(session({
-	secret: 'mean mouse',
-	resave: true,
-	saveUninitialized: true
-}));
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.use(routes);
 
